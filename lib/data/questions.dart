a586444fa0df0bc74dcc676e50b281ff788655a2
David Michael Plehn <plehndm@s.dcsdk12.org>
2024-09-10 12:25:51 -0600

    initial commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,164 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter UIs?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_2',
+    'Which folder is your code stored and run?', 
+    [
+      'lib', 
+      'build', 
+      'android', 
+      'test',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_3',
+    'How is Dart code compiled?', 
+    [
+      'Parses file from top to bottom', 
+      'Dart code is copied onto machine', 
+      'Parses file from bottom to top', 
+      'Dart code is run on the Dart Virtual Machine',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_3',
+    'Can Dart code on its own be run on devices?', 
+    [
+      'No, it needs to be compiled into the devices native language', 
+      'No, it can only run on a device that can run Dart code on its own', 
+      'Yes, Dart code is able to be run on the device the app is made for', 
+      'Yes, Dart is the native language of the device you make the app for',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_6',
+    'What do dependencies do?', 
+    [
+      'Manages all third party packages', 
+      'Manages functions', 
+      'Manages Widgets', 
+      'Allow the use of Flutter',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_7',
+    'Does the main function need to be called in the code?', 
+    [
+      'No, it is executed automatically when Dart parses file', 
+      'Yes, all functions need to be executed for their code to run', 
+      'No, it is only a placeholder for the main code in the project', 
+      'No, the code in the main function is never run',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_9',
+    'What is the root class for all widgets?', 
+    [
+      'MaterialApp', 
+      'BasicApp', 
+      'Material', 
+      'DefaultApp',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_12',
+    'What is const?', 
+    [
+      'A keyword that creates an object that refers to an existing object in memory instead of creating a new instance', 
+      'A keyword that works the same way as the final keyword', 
+      'Initializes a special object of type const', 
+      'Initializes a primitive variable',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_13',
+    'What command formats the code?', 
+    [
+      'Format Document', 
+      'Format', 
+      'Align Document', 
+      'Beautify Document',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_18',
+    'How do you identify what type a named variable takes?', 
+    [
+      'The type before the name', 
+      'Name of variable', 
+      'There is no specific type', 
+      'You cannot specify what type you want to take',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_22',
+    'What function must any class extending the StatelessWidget abstract class contain?', 
+    [
+      'build', 
+      'main', 
+      'start', 
+      'run',
+    ],
+  ),
+  QuizQuestion(
+    'Video 2_22',
+    'Do you need to call the build function?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter UIs?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter UIs?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter UIs?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    'Video 1_11',
+    'What are the main building blocks of Flutter UIs?', 
+    [
+      'Widgets', 
+      'Components', 
+      'Blocks', 
+      'Functions',
+    ],
+  ),
+];
