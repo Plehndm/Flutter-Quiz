3eb59708b90efba6908f0748d413fd1723db9ed1
David Michael Plehn <plehndm@s.dcsdk12.org>
2024-09-17 12:04:59 -0600

    Made the results screen scrollable and fit inside the screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -11,7 +11,7 @@ class ResultsScreen extends StatelessWidget {
   List<Map<String, Object>> getSummaryData() {
     final List<Map<String, Object>> summary = [];
 
-    for (var i = 0; i < chosenAnswers.length; i++){
+    for (var i = 0; i < chosenAnswers.length; i++) {
       summary.add({
         'question_index': i,
         'question': questions[i].text,
@@ -25,6 +25,12 @@ class ResultsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
+
     return Container(
       margin: const EdgeInsets.all(40),
       child: SizedBox(
@@ -32,15 +38,18 @@ class ResultsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You answered X out of Y questions correctly'),
+            Text(
+              'You answered $numCorrectQuestions out of $numTotalQuestions questions correctly',
+            ),
             const SizedBox(
               height: 30,
             ),
-            QuestionsSummary(getSummaryData()),
+            QuestionsSummary(summaryData),
             const SizedBox(
               height: 30,
             ),
             TextButton(
+              style: TextButton.styleFrom(foregroundColor: Colors.lightBlue),
               onPressed: () {},
               child: const Text('Restart Quiz'),
             ),
